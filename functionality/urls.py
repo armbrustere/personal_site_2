0a5e364d8a0d531070f34b21b6d79dad31e23e58
Ethan Armbruster <ethan.armbruster01@gmail.com>
2021-10-21 15:43:58 -0400

    Adds the personal_site files and folders to this new repo.

---
+++ functionality/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'functionality'
+urlpatterns = [
+    path('home/', views.get_home, name='home'),
+    path('blog/', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_detail,
+         name='post_detail'),
+    path('about_me/', views.get_about, name='get about me'),
+
+]
--- functionality/views.py
+++ functionality/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 # Create your views here.
+
+def get_home(request):
+    return render(request, 'html/home_2.html')
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'html/list.html', {'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug = post, status = 'published', publish__year = year, publish__month = month, publish__day = day)
+    return render(request, 'html/detail.html', {'post': post})
+
+def get_about(request):
+    return render(request, 'html/about_me.html')
