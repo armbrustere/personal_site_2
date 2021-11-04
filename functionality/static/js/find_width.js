086b1fcf0739ab9dfb70768b0daeae7a937d5d68
Ethan Armbruster <ethan.armbruster01@gmail.com>
2021-11-04 12:13:20 -0400

    Another attempt at dynamically adjusting card size for ALL cards.

--- functionality/static/js/find_width.js
+++ functionality/static/js/find_width.js
@@ -6,15 +6,21 @@ function check_width() {
     const post_grid = document.getElementById("blog_post_grid");
     const individual_posts = document.getElementById("indiv_post_card");
     document.getElementById("demo").innerHTML = "Total Width: " + screen.width + "px";
+
+
     if (screen.width <= 375) {
-        post_grid.style.gridTemplateColumns = "auto"
-        individual_posts.style.height = "400px"
-        individual_posts.style.width = "300px"
+        post_grid.style.cssText = "grid-template-columns: auto;"
+        individual_posts.style.cssText = " height: 400px; width:300px;";
+        // post_grid.style.gridTemplateColumns = "auto"
+        // individual_posts.style.height = "400px"
+        // individual_posts.style.width = "300px"
     }
     if (screen.width > 375) {
-        post_grid.style.gridTemplateColumns = "auto auto"
-        individual_posts.style.width = "500px"
-        individual_posts.style.height = "300px"
+        post_grid.style.cssText = "grid-template-columns: auto auto;"
+        individual_posts.style.cssText = " height: 300px; width:500px;";
+        // post_grid.style.gridTemplateColumns = "auto auto"
+        // individual_posts.style.width = "500px"
+        // individual_posts.style.height = "300px"
 
     }
 
