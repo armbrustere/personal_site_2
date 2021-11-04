15871714415d871608edc371ee9754475fc6b90b
Ethan Armbruster <ethan.armbruster01@gmail.com>
2021-11-04 14:58:33 -0400

    Correctly implements dynamic css with JS using for loops.

--- functionality/static/js/find_width.js
+++ functionality/static/js/find_width.js
@@ -1,27 +1,33 @@
 // Created by Ethan Armbruster 11/4/21
 // Adjusts the size of the grid columns and the size of the cards for
 // different screen sizes.
+// noinspection JSSuspiciousNameCombination
 
 function check_width() {
     const post_grid = document.getElementById("blog_post_grid");
-    const individual_posts = document.getElementById("indiv_post_card");
+    const individual_posts = document.querySelectorAll(".grid-item");
+    const card_widthNheight = "300px"
+
     document.getElementById("demo").innerHTML = "Total Width: " + screen.width + "px";
 
-
     if (screen.width <= 375) {
-        post_grid.style.cssText = "grid-template-columns: auto;"
-        individual_posts.style.cssText = " height: 400px; width:300px;";
-        // post_grid.style.gridTemplateColumns = "auto"
-        // individual_posts.style.height = "400px"
-        // individual_posts.style.width = "300px"
+        post_grid.style.gridTemplateColumns = "auto"
+        for (let i = 0; i < individual_posts.length; i++) {
+            individual_posts[i].style.height = "400px";
+            individual_posts[i].style.width = card_widthNheight;
+        }
+
+
     }
     if (screen.width > 375) {
-        post_grid.style.cssText = "grid-template-columns: auto auto;"
-        individual_posts.style.cssText = " height: 300px; width:500px;";
-        // post_grid.style.gridTemplateColumns = "auto auto"
-        // individual_posts.style.width = "500px"
-        // individual_posts.style.height = "300px"
+        post_grid.style.gridTemplateColumns = "auto auto"
+        for (let i = 0; i < individual_posts.length; i++) {
+            individual_posts[i].style.height = card_widthNheight;
+            individual_posts[i].style.width = "400px";
+        }
 
     }
 
+
 }
+
