acc93c247a37fb0ff70dad45cbda044401fb8610
Ethan Armbruster <ethan.armbruster01@gmail.com>
2021-11-10 19:13:24 -0500

    Adds rich text editor to post creation tab of admin page.

--- functionality/models.py
+++ functionality/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 
 class PublishedManager(models.Manager):
@@ -18,7 +19,8 @@ class Post(models.Model):
     title = models.CharField(max_length=500)
     slug = models.SlugField(max_length=500, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-    body = models.TextField()
+    body = RichTextField(blank=True, null=True)
+    #body = models.TextField()
     publish = models.DateTimeField(default=django.utils.timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
