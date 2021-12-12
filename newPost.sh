8f3e79165b3bd1e800513c783ae5f070c54fe382
Ethan Armbruster <ethan.armbruster01@gmail.com>
2021-12-12 17:26:24 +0000

    Updates db with new post for the week

---
+++ newPost.sh
@@ -0,0 +1,9 @@
+#Adds db to git 
+sudo git add db.sqlite3
+
+#Makes the commit for the change
+sudo git commit -m "Updates db with new post for the week"
+
+
+#Pushes the new commit to Github
+sudo git push
